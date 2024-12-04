5dbca23582c2aa5f2cfaa9e953d0007ab4e45d8f
Bryan Morrison <bryanmorrison017@gmail.com>
2024-12-03 21:22:46 -0500

    update sizing

--- src/components/CertificationsSection.tsx
+++ src/components/CertificationsSection.tsx
@@ -14,17 +14,17 @@ export default function CertificationsSection({ certifications }: Certifications
   return (
     <div className="mt-8">
       <h3 className="text-2xl font-semibold mb-4 text-white">Certifications</h3>
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
         {certifications.map((cert, index) => (
           <div key={index} className="bg-[#232323] p-2 rounded-lg border border-[#2A2A2A] hover:border-[#00FF00] transition-all duration-300">
             <Image
               src={cert.src}
               alt={cert.alt}
-              width={150}
-              height={150}
-              className="w-full h-auto max-w-[150px] mx-auto"
+              width={100}
+              height={100}
+              className="w-full h-auto max-w-[100px] mx-auto"
             />
-            <p className="bg-[#232323] rounded-full px-3 py-1 text-sm font-semibold text-[#00FF00]  text-center">{cert.title}</p>
+            <p className="bg-[#232323] rounded-full px-3 py-1 text-xs font-semibold text-[#00FF00] text-center">{cert.title}</p>
           </div>
         ))}
       </div>
