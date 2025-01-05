d6a264469c3c5748d642c46986d7867f36f9170c
bryan.morrison <bryan.morrison@avintllc.om>
2025-01-05 16:53:39 -0500

    update nextjs and vuln packages

--- next.config.ts
+++ next.config.ts
@@ -6,13 +6,6 @@ const nextConfig = {
   distDir: 'out',
   images: {
     unoptimized: true,
-  },
-  scripts: [
-    {
-      src: 'https://www.googletagmanager.com/gtag/js',
-      async: true,
-    },
-  ],
+  }
 }
  
-module.exports = nextConfig
