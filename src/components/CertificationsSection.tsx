228169d1b0e5532183a4b1412c27b9499257df0f
Bryan Morrison <bryanmorrison017@gmail.com>
2025-01-19 22:06:56 -0500

    updating cert centering

--- src/components/CertificationsSection.tsx
+++ src/components/CertificationsSection.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import React from 'react'
 
 interface Certification {
   src: string;
@@ -16,7 +17,11 @@ export default function CertificationsSection({ certifications }: Certifications
       <h3 className="text-2xl font-semibold mb-4 text-white">Certifications</h3>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
         {certifications.map((cert, index) => (
-          <div key={index} className="bg-[#232323] p-2 rounded-lg border border-[#2A2A2A] hover:border-[#00FF00] transition-all duration-300">
+          <div 
+            key={index} 
+            className={`bg-[#232323] p-2 rounded-lg border border-[#2A2A2A] hover:border-[#00FF00] transition-all duration-300
+              ${index === certifications.length - 1 && certifications.length % 2 === 1 ? "md:col-span-2 md:w-1/2 md:mx-auto" : ""}`}
+          >
             <Image
               src={cert.src}
               alt={cert.alt}
